4eb74c9102320cfe0a24d1681f62f39976005350
praveenmarish <praveenmarish0059@gmail.com>
2020-11-27 16:02:31 +0530

    seperation

--- counters/urls.py
+++ counters/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from clock.views import index
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='main_page'),
-]
+]+ static(settings.STATIC_URL)
